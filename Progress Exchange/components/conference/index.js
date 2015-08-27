9c8e1a73dff52d8ac7d53939eaa67202964efca5
Renato Teixeira <rteixeir@progress.com>
2015-08-27 13:26:41 +0000

    Changes from Screen Builder

---
+++ Progress Exchange/components/conference/index.js
@@ -0,0 +1,79 @@
+'use strict';
+
+app.conference = kendo.observable({
+    onShow: function() {},
+    afterShow: function() {}
+});
+
+// START_CUSTOM_CODE_conference
+// END_CUSTOM_CODE_conference
+(function(parent) {
+    var dataProvider = app.data.defaultProvider,
+        flattenLocationProperties = function(dataItem) {
+            var propName, propValue,
+                isLocation = function(value) {
+                    return propValue && typeof propValue === 'object' &&
+                        propValue.longitude && propValue.latitude;
+                };
+
+            for (propName in dataItem) {
+                if (dataItem.hasOwnProperty(propName)) {
+                    propValue = dataItem[propName];
+                    if (isLocation(propValue)) {
+                        // Location type property
+                        dataItem[propName] =
+                            kendo.format('Latitude: {0}, Longitude: {1}',
+                                propValue.latitude, propValue.longitude);
+                    }
+                }
+            }
+        },
+        dataSourceOptions = {
+            type: 'everlive',
+            transport: {
+                typeName: 'Conference',
+                dataProvider: dataProvider
+            },
+
+            change: function(e) {
+                var data = this.data();
+                for (var i = 0; i < data.length; i++) {
+                    var dataItem = data[i];
+
+                    flattenLocationProperties(dataItem);
+                }
+            },
+            schema: {
+                model: {
+                    fields: {
+                        'name': {
+                            field: 'name',
+                            defaultValue: ''
+                        },
+                    }
+                }
+            },
+        },
+        dataSource = new kendo.data.DataSource(dataSourceOptions),
+        conferenceModel = kendo.observable({
+            dataSource: dataSource,
+            itemClick: function(e) {
+                app.mobileApp.navigate('#components/conference/details.html?uid=' + e.dataItem.uid);
+            },
+            detailsShow: function(e) {
+                var item = e.view.params.uid,
+                    dataSource = conferenceModel.get('dataSource'),
+                    itemModel = dataSource.getByUid(item);
+                if (!itemModel.date) {
+                    itemModel.date = String.fromCharCode(160);
+                }
+                conferenceModel.set('currentItem', itemModel);
+            },
+            currentItem: null
+        });
+
+    parent.set('conferenceModel', conferenceModel);
+})(app.conference);
+
+// START_CUSTOM_CODE_conferenceModel
+// END_CUSTOM_CODE_conferenceModel
