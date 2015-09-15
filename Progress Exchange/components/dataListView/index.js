4a0597f1222286f60e0d1806290794ea1d361c31
Renato Teixeira <rteixeir@progress.com>
2015-09-15 21:22:32 +0000

    Changes from Screen Builder

---
+++ Progress Exchange/components/dataListView/index.js
@@ -0,0 +1,93 @@
+'use strict';
+
+app.dataListView = kendo.observable({
+    onShow: function() {},
+    afterShow: function() {}
+});
+
+// START_CUSTOM_CODE_dataListView
+// END_CUSTOM_CODE_dataListView
+(function(parent) {
+    var dataProvider = app.data.defaultProvider,
+        processImage = function(img) {
+            if (!img) {
+                var empty1x1png = 'iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVQI12NgYAAAAAMAASDVlMcAAAAASUVORK5CYII=';
+                img = 'data:image/png;base64,' + empty1x1png;
+            } else if (img.slice(0, 4) !== 'http' &&
+                img.slice(0, 2) !== '//' &&
+                img.slice(0, 5) !== 'data:') {
+                var setup = dataProvider.setup;
+                img = setup.scheme + ':' + setup.url + setup.apiKey + '/Files/' + img + '/Download';
+            }
+
+            return img;
+        },
+        flattenLocationProperties = function(dataItem) {
+            var propName, propValue,
+                isLocation = function(value) {
+                    return propValue && typeof propValue === 'object' &&
+                        propValue.longitude && propValue.latitude;
+                };
+
+            for (propName in dataItem) {
+                if (dataItem.hasOwnProperty(propName)) {
+                    propValue = dataItem[propName];
+                    if (isLocation(propValue)) {
+                        // Location type property
+                        dataItem[propName] =
+                            kendo.format('Latitude: {0}, Longitude: {1}',
+                                propValue.latitude, propValue.longitude);
+                    }
+                }
+            }
+        },
+        dataSourceOptions = {
+            type: 'everlive',
+            transport: {
+                typeName: 'Workshops',
+                dataProvider: dataProvider
+            },
+
+            change: function(e) {
+                var data = this.data();
+                for (var i = 0; i < data.length; i++) {
+                    var dataItem = data[i];
+
+                    flattenLocationProperties(dataItem);
+                }
+            },
+            schema: {
+                model: {
+                    fields: {
+                        'name': {
+                            field: 'name',
+                            defaultValue: ''
+                        },
+                    }
+                }
+            },
+        },
+        dataSource = new kendo.data.DataSource(dataSourceOptions),
+        dataListViewModel = kendo.observable({
+            dataSource: dataSource,
+            itemClick: function(e) {
+                app.mobileApp.navigate('#components/dataListView/details.html?uid=' + e.dataItem.uid);
+            },
+            detailsShow: function(e) {
+                var item = e.view.params.uid,
+                    dataSource = dataListViewModel.get('dataSource'),
+                    itemModel = dataSource.getByUid(item);
+                itemModel.dateUrl = processImage(itemModel.date);
+                if (!itemModel.name) {
+                    itemModel.name = String.fromCharCode(160);
+                }
+                dataListViewModel.set('currentItem', itemModel);
+            },
+            currentItem: null
+        });
+
+    parent.set('dataListViewModel', dataListViewModel);
+})(app.dataListView);
+
+// START_CUSTOM_CODE_dataListViewModel
+// END_CUSTOM_CODE_dataListViewModel
